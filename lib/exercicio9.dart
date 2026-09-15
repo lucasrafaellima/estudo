2a727c9fb256b11a455e3f07aebde00513076ea0
lucasrafaellima <lucasrafaellima03@gmail.com>
2026-09-15 09:51:00 -0300

    Refactor ContaBancaria class to use nullable titular and improve main function structure

--- lib/exercicio9.dart
+++ lib/exercicio9.dart
@@ -1,15 +1,18 @@
 import 'dart:io';
 class ContaBancaria {
-  String titular;
+  String? titular;
   double _saldo;
 
-  ContaBancaria(this.titular, double saldoInicial) : _saldo = saldoInicial;
+  ContaBancaria(String titular, double saldoInicial)
+      : titular = titular,
+        _saldo = saldoInicial;
 
   double get saldo => _saldo;
 
   set depositar(double valor) {
     if (valor > 0) {
       _saldo += valor;
+      print('O novo valor é $_saldo');
     } else {
       print('Erro: O valor do depósito deve ser maior que zero');
     }
@@ -18,17 +21,38 @@ class ContaBancaria {
   bool sacar(double valor) {
     if (valor >= 0 && valor <= _saldo) {
       _saldo -= valor;
+      print('O novo valor é: $_saldo');
       return true;
     } else {
       print('Erro, saldo insuficiente!');
       return false;
     }
   }
+}
 
-  void main() {
-    print('Informe o nome do titular: ');
-    var nome = stdin.readLineSync();
+void main() {
 
-  }
+    stdout.write('Informe o nome do titular: ');
+    String nome = stdin.readLineSync() ?? '';
+
+    stdout.write('Informe o saldo do titular: ');
+    double saldo = double.tryParse(stdin.readLineSync() ?? '') ?? 0.0;
+
+    var conta = ContaBancaria(nome, saldo);
+
+    print('O nome do titular é: $nome');
+    print('O saldo é: ${conta.saldo}');
+
+    stdout.write('Deseja sacar algum valor[S/N]?');
+    String resp = stdin.readLineSync() ?? '';
+
+    bool resp2;
+
+    if (resp == 'S') {
+      resp2 = true;
+      stdout.write('Digite o valor que deseja sacar: ');
+      double valor = double.tryParse(stdin.readLineSync() ?? '') ?? 0.0;
+      conta.sacar(valor);
+    }
 
 }
